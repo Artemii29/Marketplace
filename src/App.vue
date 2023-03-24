c315729714d7695d9ca857b9984201bc58b6772c
Artemii29 <artemii.kharitonov@bk.ru>
2023-03-24 17:38:04 +0300

    update button and input

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
-<template>
-<btn-big @click=""/>
-  <textField/>
-</template>
 <script setup>
-import BtnBig from "./components/buttons/btnBig.vue"
-import textField from "./components/inputs/textField.vue"
-
+const title = ref("Hello World");
+title.value = "Hello ";
 </script>
+
+<template>
+  <div class="flex h-full items-center justify-center">
+    <button class="h-[44px] w-[185px] rounded-sm bg-yellow text-[#000] hover:bg-yellowHover active:bg-yellowActive">
+      {{ title }}
+    </button>
+  </div>
+</template>
--- types/components.d.ts
+++ types/components.d.ts
@@ -9,14 +9,14 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    Btn_big: typeof import('~/components/buttons/btnBig.vue')['default']
-    BtnBig: typeof import('./../src/components/buttons/btnBig.vue')['default']
+    Btn_big: typeof import('~/components/buttons/MainButton.vue')['default']
+    BtnBig: typeof import('~/components/buttons/MainButton.vue')['default']
     IMdiGithub: typeof import('~icons/mdi/github')['default']
     ITablerChevronDown: typeof import('~icons/tabler/chevron-down')['default']
     ITablerColorSwatch: typeof import('~icons/tabler/color-swatch')['default']
     ITablerLanguage: typeof import('~icons/tabler/language')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
-    TextField: typeof import('./../src/components/inputs/textField.vue')['default']
+    TextField: typeof import('~/components/inputs/MainInput.vue')['default']
   }
 }
