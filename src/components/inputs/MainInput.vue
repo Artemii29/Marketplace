7c91286693fa16f75026fa2b8f606c94954f232e
Artemii29 <artemii.kharitonov@bk.ru>
2023-03-31 12:17:12 +0300

    Update input

--- src/components/inputs/MainInput.vue
+++ src/components/inputs/MainInput.vue
@@ -15,5 +15,11 @@ enum Size {
 </script>
 
 <template>
-  <input type="date" class="">
+  <input type="date" class=""
+  :class="{
+  'sm:h-[40px] sm:w-[162px] md:h-[44px] md:w-[185px]': size === Size.small,
+  'sm:h-[44px] sm:w-[185px] md:h-[48px] md:w-[200px]': size === Size.medium,
+  'sm:h-[48px] sm:w-[220px] ': size === Size.large,
+  }"
+  @input="$emit('broadcast',userInput)">
 </template>
